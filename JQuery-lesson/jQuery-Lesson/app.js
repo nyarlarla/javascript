168f1d2e74c9a9b2d8aad9cd4ad0bc4672501d5f
西雄太 <nishiyuuta@nishiyuutanoMacBook-Pro.local>
2019-06-24 12:46:37 +0900

    [Finish]7-8

---
+++ JQuery-lesson/jQuery-Lesson/app.js
@@ -0,0 +1,8 @@
+$(function(){
+	$('.box1').slideDown(function(){
+	     $('.box1').css({'height': '100px',
+		            'width':  '200px',
+                    'background-color': '#0000FF'
+                    }).slideUp();
+         });
+	});
